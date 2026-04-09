ac695b0562aef49348cde010825d748ef1f3736a
Thalles Felipe Horn <129456127+thalleshorn@users.noreply.github.com>
2026-04-09 08:18:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+// MENU RESPONSIVO
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
 
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// SCROLL SUAVE
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+// FORMULÁRIO
+const form = document.getElementById("formContato");
+const mensagem = document.getElementById("mensagem");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    mensagem.textContent = "Mensagem enviada com sucesso!";
+    mensagem.style.color = "green";
+    form.reset();
+});
